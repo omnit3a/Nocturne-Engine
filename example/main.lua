8c5d401318c592f796a7170eb13291630f62d680
omnitia <fostersloantv@gmail.com>
2024-01-13 09:59:19 -0500

    example/main.lua stuff

--- example/main.lua
+++ example/main.lua
@@ -1,9 +1,11 @@
 id = 0
+x = 0
+y = 0
 function init()
    init_renderer()
    create_window("Example Project", 256, 256)
    id = load_bmp("default.bmp", 1)
-   reshape_texture(id, 0, 0, 54, 54)
+   reshape_texture(id, x, y, 54, 54)
 end
 
 function main()
@@ -12,6 +14,16 @@ function main()
 	 break
       end	 
       
+      if (is_key_held("Right")) then
+	 x = x + 1
+	 reshape_texture(id, x, y, 54, 54)
+      end
+
+      if (is_key_held("Left")) then
+	 x = x - 1
+	 reshape_texture(id, x, y, 54, 54)
+      end
+
       draw_texture(id)
       
       redraw_window()
